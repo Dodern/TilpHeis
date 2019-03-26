a968d0283ce0aaeaef42f993db35753ff5b5566a
Dodern <jacob.hemstad.gimle@gmail.com>
2019-03-26 14:18:42 +0100

    fixed lights.h

--- source/lights.h
+++ source/lights.h
@@ -18,7 +18,7 @@ void Lights__setFloorLights(int floor);
 /**
 * @brief Sets the lights inside the elevator. 
 * @param[in] button Type of button pressed (BUTTON_CALL_UP, BUTTON_CALL_DOWN or BUTTON_COMMAND)
-* @param[in] floor The current floor of the elevator.
+* @param[in] floor Desired floor.
 * @param[in] value Turn light on or off. 0 for off, other values for on.
 */
 void Lights__setInsideLights(elev_button_type_t button, int floor, int value);
@@ -26,23 +26,19 @@ void Lights__setInsideLights(elev_button_type_t button, int floor, int value);
 /**
 * @brief Sets the lights on each floor outside the elevator.
 * @param[in] button Type of button pressed (BUTTON_CALL_UP, BUTTON_CALL_DOWN or BUTTON_COMMAND)
-* @param[in] floor The current floor of the elevator.
+* @param[in] floor Desired floor.
 * @param[in] value Turn light on or off. 0 for off, other values for on.
 */
 void Lights__setOutsideLights(elev_button_type_t button, int floor, int value);
 
 /**
 * @brief Sets the door indicator light.
-* @param[in] button Type of button pressed (BUTTON_CALL_UP, BUTTON_CALL_DOWN or BUTTON_COMMAND)
-* @param[in] floor The current floor of the elevator.
 * @param[in] value Turn light on or off. 0 for off, other values for on.
 */
 void Lights__setDoorLight(int value);
 
 /**
 * @brief Sets the stop indicator light.
-* @param[in] button Type of button pressed (BUTTON_CALL_UP, BUTTON_CALL_DOWN or BUTTON_COMMAND)
-* @param[in] floor The current floor of the elevator.
 * @param[in] value Turn light on or off. 0 for off, other values for on.
 */
 void Lights__setStopLight(int value);
