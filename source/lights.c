d7212628c73bba2037e52eff99f5a220406cf8f1
Dodern <jacob.hemstad.gimle@gmail.com>
2019-03-26 14:42:55 +0100

    fixed lights

--- source/lights.c
+++ source/lights.c
@@ -1,12 +1,9 @@
 #include "lights.h"
 
-void Lights__setFloorLights(int floor){
+void Lights__setCurrentFloorLights(int floor){
     elev_set_floor_indicator(floor);
 }
-void Lights__setInsideLights(elev_button_type_t button, int floor, int value){
-    elev_set_button_lamp(button, floor, value);
-}
-void Lights__setOutsideLights(elev_button_type_t button, int floor, int value){
+void Lights__setDesiredFloorLights(elev_button_type_t button, int floor, int value){
     elev_set_button_lamp(button, floor, value);
 }
 void Lights__setDoorLight(int value){
--- source/lights.h
+++ source/lights.h
@@ -13,7 +13,7 @@
 * @brief Sets the floor indicator lights.
 * @param[in] floor The current floor of the elevator
 */
-void Lights__setFloorLights(int floor);
+void Lights__setCurrentFloorLights(int floor);
 
 /**
 * @brief Sets the lights inside the elevator. 
@@ -21,15 +21,7 @@ void Lights__setFloorLights(int floor);
 * @param[in] floor Desired floor.
 * @param[in] value Turn light on or off. 0 for off, other values for on.
 */
-void Lights__setInsideLights(elev_button_type_t button, int floor, int value);
-
-/**
-* @brief Sets the lights on each floor outside the elevator.
-* @param[in] button Type of button pressed (BUTTON_CALL_UP, BUTTON_CALL_DOWN or BUTTON_COMMAND)
-* @param[in] floor Desired floor.
-* @param[in] value Turn light on or off. 0 for off, other values for on.
-*/
-void Lights__setOutsideLights(elev_button_type_t button, int floor, int value);
+void Lights__setDesiredFloorLights(elev_button_type_t button, int floor, int value);
 
 /**
 * @brief Sets the door indicator light.
